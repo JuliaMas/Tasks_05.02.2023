2774b0fed6505e69ae39b064d504c643a61c8c08
Julia <julia.masaeva@gmail.com>
2023-02-04 20:01:52 +0600

    В матрице чисел найти сумму элементов главной диагонали

---
+++ Task004/Program.cs
@@ -0,0 +1,48 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+int [,] CreateMatrix(int m, int n)
+{
+    var matrix = new int[m,n];
+    var random = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = random.Next(10,100);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите кол-во строк: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int n = int.Parse(Console.ReadLine()!);
+var matrix = CreateMatrix(m,n);
+int sum = 0;
+PrintMatrix(matrix);
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if(i == j)
+        {
+            sum = sum + matrix[i,j]; 
+            
+        }
+    }
+ }
+ Console.WriteLine("Сумма элементов главной диагонали равна " + sum);
+
+
