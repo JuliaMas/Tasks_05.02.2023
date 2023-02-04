51023021fefedd5cac2adc473450c9419bae96ad
Julia <julia.masaeva@gmail.com>
2023-02-04 16:05:24 +0600

    Задать двумерный массив следующим правилом: A[mn] = m+n

---
+++ Task002/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+int [,] CreateMatrix(int m, int n)
+{
+    var matrix = new int[m,n];
+    //var random = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = i + j;
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите кол-во столбцов: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во строк: ");
+int n = int.Parse(Console.ReadLine()!);
+PrintMatrix(CreateMatrix(m,n));
